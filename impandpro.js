112db818cbc92100fe28c6f5fb9ba33d72ac7a3f
Grant <grant.spedding@live.com>
2024-12-02 14:59:31 +1100

    Create impandpro.js
    initial setup.
    No import or output set up.
    Just the image manipulation and OCR test

---
+++ impandpro.js
@@ -0,0 +1,42 @@
+import os
+import cv2
+import pytesseract
+import numpy as np
+from pytesseract import Output
+from PIL import Image, ImageOps
+
+#Variables
+size = (480, 40)
+source = (0, 620, 480, 680)
+dest = (0, 310, 480, 370)
+
+
+#Capture image
+
+
+
+
+
+
+#Process OCR
+
+im = Image.open("capture.png")
+
+region = im.crop(source)
+
+im.paste(region, dest)
+
+ImageOps.fit(im, size).save("idblock.png")
+
+img = cv2.imread("idblock.png")
+
+carddetails = pytesseract.image_to_string(img)
+
+print(carddetails) #Remove after debugging complete
+
+os.remove("idblock.png")
+
+os.remove("capture.png")
+
+
+#Process the string to an array
